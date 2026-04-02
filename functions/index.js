78e59bfda27f068c2084118239d1f65383399eb9
istosmedical <mazhar@istos.in>
2026-04-02 17:04:03 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,33 @@
+const functions = require("firebase-functions");
+const Razorpay = require("razorpay");
+
+const razorpay = new Razorpay({
+  key_id: "YOUR_RAZORPAY_KEY_ID",
+  key_secret: "YOUR_RAZORPAY_SECRET"
+});
+
+// Cloud Function to create Razorpay order
+exports.createOrder = functions.https.onRequest(async (req, res) => {
+  const options = {
+    amount: 50000, // example: Rs. 500.00
+    currency: "INR",
+    receipt: "receipt#1"
+  };
+
+  try {
+    const order = await razorpay.orders.create(options);
+    res.json(order);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// Webhook to handle payment status
+exports.paymentWebhook = functions.https.onRequest((req, res) => {
+  const event = req.body;
+  console.log("Payment webhook received:", event);
+
+  // TODO: update Firestore with payment status
+  res.sendStatus(200);
+});
 
